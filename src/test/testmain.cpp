4dbf6fcd5d703e9151da96f9984395b2f414941c
Hyunsang Youn <wizehack@gmail.com>
2016-12-06 07:25:59 -0500

    Revert "[test] Register Channel Test Suite"
    This reverts commit 2258c411e4ce8f8c4ea3d23ab763d7dc48d02013.
    
    Change-Id: Idb738bdb25960489a405791f98c395fa7a4c32b8

--- src/test/testmain.cpp
+++ src/test/testmain.cpp
@@ -15,20 +15,20 @@ int main(int argc, char **argv)
 
     if(opt)
     {
-//        std::cout << "######### START TEST #########" << std::endl;
+        std::cout << "######### START TEST #########" << std::endl;
 
         tester = new Tester();
         isSuccess = tester->execute(opt);
 
         if(isSuccess)
-            std::cout << opt->getCategory() << " " << opt->getTestCaseID() << ": Success" << std::endl;
+            std::cout << "Result: Success" << std::endl;
         else
-            std::cout << opt->getCategory() << " " << opt->getTestCaseID() << ": Failure" << std::endl;
+            std::cout << "Result: Failure" << std::endl;
 
         delete tester;
         delete opt;
 
-//        std::cout << "########## END TEST ##########" << std::endl;
+        std::cout << "########## END TEST ##########" << std::endl;
     }
     else
         std::cout << "NOT tested" << std::endl;
