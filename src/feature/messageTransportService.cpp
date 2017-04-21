338a54769a2f5c3aa6f875b74bb03b5eaea2f656
wizehack <wizehack@gmail.com>
2017-04-22 06:15:22 +0900

    [test] add unit test flag
    :Release:
    add unit test flag
    
    :Details:
    Flag FEAT_TEST_MODE is added.
    It is one of the build option to enable or disable unit test flag
    
    :Test:
    Build Test & Unit Test
    
    :Issues:
    N/A
    
    :Reference:
    N/A
    
    Change-Id: I2097e267d46d22bba944f428f8eb93d807503687

--- src/feature/messageTransportService.cpp
+++ src/feature/messageTransportService.cpp
@@ -5,5 +5,7 @@ mcHubd::MessageTransportService::MessageTransportService(){}
 mcHubd::MessageTransportService::~MessageTransportService(){}
 void mcHubd::MessageTransportService::sendAll(std::string message)
 {
+#if FEAT_TEST_MODE
     mcHubd::TestStub::getInstance()->addSubscribeMsg(message); //Test Code
+#endif
 }
